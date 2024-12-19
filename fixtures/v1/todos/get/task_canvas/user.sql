7ad0172927bef207052ec24a677dda44eb3b8b47
kamegoro <tentyo0620@icloud.com>
2024-12-19 21:59:41 +0900

    update test case

--- fixtures/v1/todos/get/task_canvas/user.sql
+++ fixtures/v1/todos/get/task_canvas/user.sql
@@ -1,3 +1,4 @@
 INSERT INTO task_canvas.user (id, email, password_hash) VALUES
-    ('F32FE27B-E5A4-4F2B-AA02-1D91A977B1D8', 'test@example.com', '$2a$10$e4MBDWHc1fe/eg3d1zNyCOMxH8TtBKvv5VODZ5w7SNbxa79r6Olke')
+    ('F32FE27B-E5A4-4F2B-AA02-1D91A977B1D8', 'test@example.com', '$2a$10$e4MBDWHc1fe/eg3d1zNyCOMxH8TtBKvv5VODZ5w7SNbxa79r6Olke'),
+    ('5CB1EDE3-A716-4BE1-A652-F95F5963FDF6', 'test2@example.com', '$2a$10$e4MBDWHc1fe/eg3d1zNyCOMxH8TtBKvv5VODZ5w7SNbxa79r6Olke')
 ;
