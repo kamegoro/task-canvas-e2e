97fdf3dcba65460eaa54f23c9aa99aaa80cc9451
kamegoro <tentyo0620@icloud.com>
2024-12-20 19:17:42 +0900

    add delete todo setup

---
+++ fixtures/v1/todos/delete/task_canvas/todo.sql
@@ -0,0 +1,4 @@
+INSERT INTO task_canvas.todo (id, content, completed) VALUES
+    ('C6F3B847-0919-4A57-B96E-2993B0414464', 'TODOのコンテンツ内容', false),
+    ('03FF5762-CA9A-4FD6-83C1-B1B0B50517BA', '取得したくないTODO', false)
+;
