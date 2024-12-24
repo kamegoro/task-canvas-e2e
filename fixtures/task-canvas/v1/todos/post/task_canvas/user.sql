7ece1a5497d3c27ef0ed5a4e629d0ff4daabeb09
kamegoro <tentyo0620@icloud.com>
2024-12-24 20:30:35 +0900

    update pom

---
+++ fixtures/task-canvas/v1/todos/post/task_canvas/user.sql
@@ -0,0 +1,3 @@
+INSERT INTO task_canvas.user (id, email, password_hash) VALUES
+    ('F32FE27B-E5A4-4F2B-AA02-1D91A977B1D8', 'test@example.com', '$2a$10$e4MBDWHc1fe/eg3d1zNyCOMxH8TtBKvv5VODZ5w7SNbxa79r6Olke')
+;
---
+++ src/test/kotlin/com.taskcanvas/TaskCanvasTagManager.kt
@@ -0,0 +1,4 @@
+package com.taskcanvas
+
+class TaskCnavasTagManager {
+}
