f10bddd484ef1a70f6ed3293e83b92519b37001b
kamegoro <tentyo0620@icloud.com>
2024-12-25 19:40:46 +0900

    update deleteTag test

--- fixtures/task-canvas-tag-manager/v1/tags/get/task_canvas/tag.sql
+++ fixtures/task-canvas-tag-manager/v1/tags/get/task_canvas/tag.sql
@@ -1,3 +1,4 @@
 INSERT INTO task_canvas.tag (id, name, is_deleted) VALUES
-    ('97E85013-EE6A-4A6A-BDB9-4B195E13DFAB', 'tag1', false)
+    ('97E85013-EE6A-4A6A-BDB9-4B195E13DFAB', 'tag1', false),
+    ('4EA30957-FAC2-4271-9C4D-A22736A2A697', '削除済みのタグ', true)
 ;
