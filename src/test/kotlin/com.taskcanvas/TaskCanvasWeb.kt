91fd3178a2560a2bb07af6d0924adce278af1816
kamegoro <tentyo0620@icloud.com>
2025-01-13 04:11:16 +0900

    add TaskCanvasWeb.kt

--- src/test/kotlin/com.taskcanvas/TaskCanvasWeb.kt
+++ src/test/kotlin/com.taskcanvas/TaskCanvasWeb.kt
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Condition.attribute
 import com.codeborne.selenide.Condition.visible
 import com.codeborne.selenide.Selenide.open
 import com.thoughtworks.gauge.Step
+import net.bytebuddy.asm.Advice.Local
 
 class TaskCanvasWeb {
     @Step("サインインページにアクセスする")
@@ -46,4 +47,43 @@ class TaskCanvasWeb {
     fun サインアップへの遷移リンクが表示されている() {
         Locator.getByRole(Role.Button, "サインアップ").first().shouldBe(visible)
     }
+
+    @Step("メールの入力欄に<email>を入力する")
+    fun メールアドレスに値を入力する(email: String) {
+        Locator.getByRoleAll(Role.Textbox)
+            .filter(attribute("type", "email"))
+            .filter(attribute("placeholder", "メールを入力してください"))
+            .first()
+            .sendKeys(email)
+    }
+
+    @Step("パスワードの入力欄に<password>を入力する")
+    fun パスワードに値を入力する(password: String) {
+        Locator.getByRoleAll(Role.Textbox)
+            .filter(attribute("type", "password"))
+            .filter(attribute("placeholder", "パスワードを入力してください"))
+            .first()
+            .sendKeys(password)
+    }
+
+    @Step("サインインボタンをクリックする")
+    fun サインインボタンをクリックする() {
+        Locator.getByRole(Role.Button, "サインイン").first().click()
+    }
+
+    @Step("ローディングが表示される")
+    fun ローディングが表示される() {
+        Locator.getByRole(Role.Alert, "ローディング").first().shouldBe(visible)
+    }
+
+    @Step("サインインの成功通知が表示される")
+    fun サインインの成功通知が表示される() {
+        Locator.getByRole(Role.Alert, "ログインに成功しました").first().shouldBe(visible)
+    }
+
+    @Step("TODO一覧ページが表示されている")
+    fun Todo一覧ページが表示されている() {
+        Locator.getByRole(Role.Paragraph, "My Todo-s").first().shouldBe(visible)
+    }
 }
+
