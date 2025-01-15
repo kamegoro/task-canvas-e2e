8d08a712350274b9a61deb45277b49c15aff893a
kamegoro <tentyo0620@icloud.com>
2025-01-15 23:23:25 +0900

    add TaskCanvasWeb for wiremock

--- src/test/kotlin/com.taskcanvas/Mock.kt
+++ src/test/kotlin/com.taskcanvas/Mock.kt
@@ -1,18 +1,5 @@
 package com.taskcanvas
 
-import com.github.tomakehurst.wiremock.WireMockServer
 import com.github.tomakehurst.wiremock.client.WireMock
-import com.github.tomakehurst.wiremock.core.WireMockConfiguration
-import java.io.File
 
-object TaskCanvasApiForWeb {
-    private val mockInstance: WireMock = WireMock(config.taskCanvasWeb.backendHost, config.taskCanvasWeb.backendPort)
-
-    fun resetToDefaultMappings() {
-        mockInstance.resetToDefaultMappings()
-    }
-
-    fun loadMapping(file: File) {
-        mockInstance.loadMappingsFrom(file)
-    }
-}
+object TaskCanvasApiForWeb: WireMock(config.taskCanvasWeb.backendHost, config.taskCanvasWeb.backendPort)
